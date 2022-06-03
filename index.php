f0abaa616cbecccd87583da3f857abd79f77f9ad
jang walia <jangiwalia83@gmail.com>
2022-06-03 09:45:32 -0700

    create header file for modular approach

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<?php 
-  require_once("includes/classes/config.php");
-  require_once("includes/classes/PreviewProvider.php");
-  require_once("includes/classes/Entity.php");
-  if(!isset($_SESSION["userLoggedIn"])) {
-    header("Location: register.php");
-  }
-  $loggedInUser = $_SESSION["userLoggedIn"];
+<?php
+  require_once("includes/Header.php");
 
   $preview = new PreviewProvider($conn,$loggedInUser);
 
