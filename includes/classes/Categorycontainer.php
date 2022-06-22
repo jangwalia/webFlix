aa84c87f4f599c6099c7b360bf966711822d2f50
jang walia <jangiwalia83@gmail.com>
2022-06-22 09:58:40 -0700

    create Categorycontainer class

--- includes/classes/Categorycontainer.php
+++ includes/classes/Categorycontainer.php
@@ -12,13 +12,25 @@
       $html = "<div class = 'categoriesList'>";
 
       while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        $html .= $this->showcategoriesList($row,null,true,true);
+        $html .= $this->showcategoriesList($row,NULL,true,true);
       }
 
       return $html . "</div>";
     
     }
+    public function Showcategory($categoryID,$title = null){
+      $query = $this->conn->prepare("SELECT * FROM categories WHERE id=:id");
+      $query->bindvalue(":id",$categoryID);
+      $query->execute();
+      $html = "<div class = 'categoriesList noScroll'>";
 
+      while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        $html .= $this->showcategoriesList($row,$title,true,true);
+      }
+
+      return $html . "</div>";
+
+    }
     private function showcategoriesList($sqlData,$title,$tvShows,$movies) {
       $categoryID = $sqlData['id'];
       $title = $title == null ? $sqlData["name"] : $title;
